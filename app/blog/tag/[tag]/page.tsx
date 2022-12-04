a83cfb62fe26d5ac39857e8929fde4175292d8f8
herohoro <mtwd.office.25@gmail.com>
2022-12-05 01:20:42 +0900

    _Define BlogTagPage

--- app/blog/tag/[tag]/page.tsx
+++ app/blog/tag/[tag]/page.tsx
@@ -3,12 +3,16 @@ import { NUMBER_OF_POSTS_PER_PAGE } from '../../../../app/server-constants'
 import {
   BlogPostLink,
   BlogTagLink,
+  BlogCategoryLink,
   NextPageLink,
   PostDate,
   PostExcerpt,
   PostTags,
+  PostCategory,
   PostTitle,
   ReadMoreLink,
+  PostPerson,
+  TwitterTimeline,
 } from '../../../../components/blog-parts'
 import styles from '../../../../styles/blog.module.css'
 import {
@@ -17,14 +21,16 @@ import {
   getPostsByTag,
   getFirstPostByTag,
   getAllTags,
+  getAllCategorys,
 } from '../../../../lib/notion/client'
+import { Link as Scroll } from 'react-scroll'
 
 export const revalidate = 60
 export const dynamicParams = false
 
 export async function generateStaticParams() {
   const tags = await getAllTags()
-  return tags.map(tag => ({ tag: tag }))
+  return tags.map((tag) => ({ tag: tag }))
 }
 
 const BlogTagPage = async ({ params: { tag: encodedTag } }) => {
@@ -36,42 +42,65 @@ const BlogTagPage = async ({ params: { tag: encodedTag } }) => {
     notFound()
   }
 
-  const [firstPost, rankedPosts, recentPosts, tags] = await Promise.all([
-    getFirstPostByTag(tag),
-    getRankedPosts(),
-    getPosts(5),
-    getAllTags(),
-  ])
+  const [firstPost, rankedPosts, recentPosts, tags, categorys] =
+    await Promise.all([
+      getFirstPostByTag(tag),
+      getRankedPosts(),
+      getPosts(5),
+      getAllTags(),
+      getAllCategorys(),
+    ])
 
   return (
     <div className={styles.container}>
-      <div className={styles.mainContent}>
-        <header>
-          <h2>{tag}</h2>
-        </header>
+      <div className={styles.flexWraper}>
+        <div className={styles.mainContent}>
+          <header>
+            <span style={{ marginRight: '1rem' }}>tag:</span>
+            <h2 style={{ marginLeft: '3rem' }}>{tag}</h2>
+          </header>
 
-        {posts.map(post => {
-          return (
-            <div className={styles.post} key={post.Slug}>
-              <PostDate post={post} />
-              <PostTags post={post} />
-              <PostTitle post={post} />
-              <PostExcerpt post={post} />
-              <ReadMoreLink post={post} />
-            </div>
-          )
-        })}
+          {posts.map((post) => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostCategory post={post} />
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostPerson post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
 
-        <footer>
-          <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
-        </footer>
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} tag={tag} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <BlogCategoryLink heading="Categorys" categorys={categorys} />
+          <BlogTagLink heading="Tags" tags={tags} />
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+          <BlogPostLink heading="Latest Posts" posts={recentPosts} />
+        </div>
       </div>
-
-      <div className={styles.subContent}>
-        <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        <BlogPostLink heading="Latest Posts" posts={recentPosts} />
-        <BlogTagLink heading="Categories" tags={tags} />
+      <div className={styles.endContent}>
+        <div className={styles.endSection}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+        </div>
+        <div className={styles.endSection}>
+          <BlogCategoryLink heading="Category List" categorys={categorys} />
+          <TwitterTimeline />
+        </div>
+        <div className={styles.endSection}>
+          <BlogTagLink heading="Tag List" tags={tags} />
+        </div>
       </div>
+      <Scroll to="topJump" className={styles.topJump} smooth={true}>
+        Top
+      </Scroll>
     </div>
   )
 }
