7218f9ef2a3e6323d587b68a94e4c2199c4b0eff
herohoro <mtwd.office.25@gmail.com>
2022-12-05 00:28:45 +0900

    _Define BlogPage

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,13 +2,17 @@ import { NUMBER_OF_POSTS_PER_PAGE } from '../../app/server-constants'
 import {
   BlogPostLink,
   BlogTagLink,
+  BlogCategoryLink,
   NextPageLink,
   NoContents,
   PostDate,
   PostExcerpt,
   PostTags,
+  PostCategory,
   PostTitle,
   ReadMoreLink,
+  TwitterTimeline,
+  PostPerson,
 } from '../../components/blog-parts'
 import styles from '../../styles/blog.module.css'
 import {
@@ -16,44 +20,70 @@ import {
   getFirstPost,
   getRankedPosts,
   getAllTags,
+  getAllCategorys,
 } from '../../lib/notion/client'
+import { Link as Scroll } from 'react-scroll'
+import DocumentHead from '../../components/document-head'
 
 export const revalidate = 60
 
 const BlogPage = async () => {
-  const [posts, firstPost, rankedPosts, tags] = await Promise.all([
+  const [posts, firstPost, rankedPosts, tags, categorys] = await Promise.all([
     getPosts(NUMBER_OF_POSTS_PER_PAGE),
     getFirstPost(),
     getRankedPosts(),
     getAllTags(),
+    getAllCategorys(),
   ])
 
   return (
     <div className={styles.container}>
-      <div className={styles.mainContent}>
-        <NoContents contents={posts} />
+      <DocumentHead title="AllPost" />
+      <div className={styles.flexWraper}>
+        <div className={styles.mainContent}>
+          <NoContents contents={posts} />
 
-        {posts.map(post => {
-          return (
-            <div className={styles.post} key={post.Slug}>
-              <PostDate post={post} />
-              <PostTags post={post} />
-              <PostTitle post={post} />
-              <PostExcerpt post={post} />
-              <ReadMoreLink post={post} />
-            </div>
-          )
-        })}
+          {posts.map((post) => {
+            return (
+              <div className={styles.post} key={post.Slug}>
+                <PostCategory post={post} />
+                <PostDate post={post} />
+                <PostTags post={post} />
+                <PostTitle post={post} />
+                <PostPerson post={post} />
+                <PostExcerpt post={post} />
+                <ReadMoreLink post={post} />
+              </div>
+            )
+          })}
 
-        <footer>
-          <NextPageLink firstPost={firstPost} posts={posts} />
-        </footer>
+          <footer>
+            <NextPageLink firstPost={firstPost} posts={posts} />
+          </footer>
+        </div>
+
+        <div className={styles.subContent}>
+          <TwitterTimeline />
+          <BlogCategoryLink heading="Categorys" categorys={categorys} />
+          <BlogTagLink heading="Tags" tags={tags} />
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+        </div>
       </div>
-
-      <div className={styles.subContent}>
-        <BlogPostLink heading="Recommended" posts={rankedPosts} />
-        <BlogTagLink heading="Categories" tags={tags} />
+      <div className={styles.endContent}>
+        <div className={styles.endSection}>
+          <BlogPostLink heading="Recommended" posts={rankedPosts} />
+        </div>
+        <div className={styles.endSection}>
+          <BlogCategoryLink heading="Category List" categorys={categorys} />
+          <TwitterTimeline />
+        </div>
+        <div className={styles.endSection}>
+          <BlogTagLink heading="Tag List" tags={tags} />
+        </div>
       </div>
+      <Scroll to="topJump" className={styles.topJump} smooth={true}>
+        Top
+      </Scroll>
     </div>
   )
 }
