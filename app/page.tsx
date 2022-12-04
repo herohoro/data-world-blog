e42b23d51b147c8a2371a3e347b0f497f52e2759
herohoro <mtwd.office.25@gmail.com>
2022-12-04 23:56:03 +0900

    データ取得は別メソッド_home

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,27 @@
-import Link from 'next/link'
 import { NEXT_PUBLIC_SITE_TITLE } from './server-constants'
 import GoogleAnalytics from '../components/google-analytics'
 import styles from '../styles/page.module.css'
+import { INDEX_PAGE_ID } from '../app/server-constants'
+import NotionBlocks from '../components/notion-block'
+import { getAllBlocksByBlockId } from '../lib/notion/client'
+import { Link as Scroll } from 'react-scroll'
 
-const RootPage = () => (
-  <>
-    <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
-    <div className={styles.container}>
-      <div>
-        <h2>Welcome!</h2>
-        <p>Your easy-notion-blog deployed successfully!</p>
-        <p>Have fun!</p>
-        <p>
-          easy-notion-blog powered by{' '}
-          <Link href="https://github.com/otoyo/easy-notion-blog">
-            otoyo/easy-notion-blog
-          </Link>
-        </p>
+export const revalidate = 60
+
+const RootPage = async () => {
+  const [blocks] = await Promise.all([getAllBlocksByBlockId(INDEX_PAGE_ID)])
+
+  return (
+    <>
+      <GoogleAnalytics pageTitle={NEXT_PUBLIC_SITE_TITLE} />
+      <div className={styles.container}>
+        <NotionBlocks blocks={blocks} />
+        <Scroll to="topJump" className={styles.topJump} smooth={true}>
+          Top
+        </Scroll>
       </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default RootPage
