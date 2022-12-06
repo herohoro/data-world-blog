57ce51749c2f11bc9601e79fe08f67af88faf8e4
herohoro <mtwd.office.25@gmail.com>
2022-12-06 12:42:18 +0900

    _Fix pagenation

--- components/blog-parts.tsx
+++ components/blog-parts.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-
+import React from 'react'
 import { Post } from '../lib/notion/interfaces'
 import NotionBlocks from './notion-block'
 import MokujiBlocks from './mokuji-block'
