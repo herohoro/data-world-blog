50f27b89a64ac829c3698ba2de33307f807dfdce
otoyo <hiroki.t.1988@gmail.com>
2022-12-24 15:02:11 +0900

    Add prefix to html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import '../styles/syntax-coloring.css'
 import styles from '../styles/shared.module.css'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
+  <html lang="ja" prefix="og: https://ogp.me/ns#">
     <body>
       <div className={styles.container}>
         <Header />
