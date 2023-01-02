5cffd042dad4ada5258dfa234d79b88e80014521
otoyo <hiroki.t.1988@gmail.com>
2023-01-02 17:22:29 +0900

    Move path module include to blog-index-cache.js

--- app/server-constants.js
+++ app/server-constants.js
@@ -1,15 +1,12 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 // use commonjs so it can be required without transpiling
 
-const path = require('path')
-
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_SITE_TITLE = 'DATA-WORLD-BLOG'
 const NEXT_PUBLIC_SITE_DESCRIPTION = "Let's learn Japanese and English! then..."
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
-const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 const INDEX_PAGE_ID = process.env.INDEX_PAGE_ID
 const TEROBI_PAGE_ID = process.env.TEROBI_PAGE_ID
@@ -24,7 +21,6 @@ module.exports = {
   NEXT_PUBLIC_SITE_TITLE,
   NEXT_PUBLIC_SITE_DESCRIPTION,
   NEXT_PUBLIC_GA_TRACKING_ID,
-  BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
   INDEX_PAGE_ID,
   TEROBI_PAGE_ID,
